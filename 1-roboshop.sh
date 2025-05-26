96ee7e1a6e9a6625b2f0ea0af112b0b4688c18e2
venu <venubonkuri1234@gmail.com>
2025-05-26 15:48:52 +0530

    logs

---
+++ 1-roboshop.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-059521b9a81665a87"
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+ZONE_ID="Z07315751QJ0K1IVQMBBK"
+DOMAIN_NAME="newgenrobots.site"
+
+for instance in ${INSTANCES[@]}
+do
+INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-059521b9a81665a87 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+
+if [ $instance != "frontend" ]
+then
+    IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+else
+    IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+fi
+    echo "$instance ip address: $IP"
+done 
