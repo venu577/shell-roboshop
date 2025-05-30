77fb56b465919fbd075f0c3055df196d78761c86
venu <venubonkuri1234@gmail.com>
2025-05-30 15:14:30 +0530

    instances created

---
+++ frontend.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+START_TIME=$(date +%s)
+# This script installs MongoDB on a Linux system
+USERID=$(id -u)
+# Check if the script is run with root access
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+#colours R is for error G is for success Y is for installation N is for normal text
+
+LOGS_FOLDER="/var/log/shellscript-logs" 
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+# Extract the script name without the extension
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+# Define the log file path
+# Create the logs folder if it doesn't exist 
+
+mkdir -p $LOGS_FOLDER
+echo "script started executing at: $(date)" &>>$LOG_FILE
+
+if [ $USERID -ne 0 ]
+
+then
+    echo -e "$R error: run with root access $N" | tee -a $LOG_FILE
+    exit 1
+    else 
+    echo -e "$G you are running with root access $N" | tee -a $LOG_FILE
+    fi
+
+    VALIDATE(){
+        if [ $1 -eq 0 ]
+        then 
+         echo -e "installing $2 is $G success $N" | tee -a $LOG_FILE
+         else 
+         echo -e "installing $2 is $R not success $N" | tee -a $LOG_FILE
+         exit 1
+         fi
+        }
+
+   dnf module disable nginx -y &>>$LOG_FILE
+   VALIDATE $? "disabling nginx module"
+
+   dnf module enable nginx:1.24 -y &>>$LOG_FILE
+   VALIDATE $? "enabling nginx version 1.24 module"
+
+   dnf install nginx -y &>>$LOG_FILE
+   VALIDATE $? "installing nginx"
+
+   systemctl enable nginx &>>$LOG_FILE
+   systemctl start nginx  &>>$LOG_FILE
+    VALIDATE $? "enabling and starting nginx service"
+
+    rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
+    VALIDATE $? "removing default nginx html files"
+
+    curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
+    VALIDATE $? "downloading frontend zip file"
+
+    cd /usr/share/nginx/html &>>$LOG_FILE
+    unzip /tmp/frontend.zip &>>$LOG_FILE
+    VALIDATE $? "unzipping frontend files"
+
+    rm -rf /etc/nginx/nginx.conf &>>$LOG_FILE
+    cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf &>>$LOG_FILE
+    VALIDATE $? "copying custom nginx config file"
+
+    systemctl restart nginx &>>$LOG_FILE
+    VALIDATE $? "restarting nginx service"
+
+    END_TIME=$(date +%s)
+     TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+    echo -e "script execution completed successfully , $Y time taken : $TOTAL_TIME Sec $N"
+        
