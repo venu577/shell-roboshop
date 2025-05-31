1ad3fc7316b590738806bf3ccffb28797bfde037
venu <venubonkuri1234@gmail.com>
2025-05-31 11:42:48 +0530

    instances created

--- frontend.sh
+++ frontend.sh
@@ -74,6 +74,5 @@ VALIDATE $? "restarting nginx service"
 
 END_TIME=$(date +%s)
 TOTAL_TIME=$(( $END_TIME - $START_TIME ))
-
 echo -e "script execution completed successfully , $Y time taken : $TOTAL_TIME Sec $N"
         
