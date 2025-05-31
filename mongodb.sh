a7ceabec8b876c200206729f539470c2f5f7a598
venu <venubonkuri1234@gmail.com>
2025-05-31 09:25:42 +0530

    instances created

--- mongodb.sh
+++ mongodb.sh
@@ -60,4 +60,5 @@ then
 
         END_TIME=$(date +%s)
         TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+        
         echo -e "script execution completed successfully , $Y time taken : $TOTAL_TIME Sec $N"
