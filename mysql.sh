33d99f5ad71f485fa4577e94dc08e055794d5791
venu <venubonkuri1234@gmail.com>
2025-05-31 10:51:43 +0530

    instances created

--- mysql.sh
+++ mysql.sh
@@ -9,6 +9,7 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE
@@ -47,7 +48,8 @@ VALIDATE $? "Starting MySQL Service"
 mysql_secure_installation --set-root-pass $MYSQL_ROOT_PASSWORD &>>$LOG_FILE
 VALIDATE $? "setting mysql root password"
 
- END_TIME=$(date +%s)
- TOTAL_TIME=$(( $END_TIME - $START_TIME ))
-  echo -e "script execution completed successfully , $Y time taken : $TOTAL_TIME Sec $N"
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+
+echo -e "script execution completed successfully , $Y time taken : $TOTAL_TIME Sec $N"
 
