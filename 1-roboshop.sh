69f7c024aeec9c5b9ada8fe258bd0486c2ebc571
venu <venubonkuri1234@gmail.com>
2025-05-31 09:14:26 +0530

    instances created

--- 1-roboshop.sh
+++ 1-roboshop.sh
@@ -18,8 +18,8 @@ then
 else
     IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
     RECORD_NAME="$DOMAIN_NAME"
-    
 fi
+
     echo "$instance ip address: $IP"
 
 aws route53 change-resource-record-sets \
